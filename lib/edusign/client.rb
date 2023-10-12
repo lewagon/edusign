5580f602c7c21d99be7fee2a2691f5e4e6fe447a
Edward Schults <edward.schults@gmail.com>
2023-10-12 09:05:56 +0200

    gst

--- lib/edusign/client.rb
+++ lib/edusign/client.rb
@@ -248,7 +248,7 @@ module Edusign
 
       response = create_professor(first_name: first_name, last_name: last_name, email: email)
       response.result
-    rescue ProfessorAlreadyExistsError::Error
+    rescue Response::ProfessorAlreadyExistsError
       # NOTE(Eschults): try again finding the professor
       find_or_create_professor(first_name: first_name, last_name: last_name, email: email)
     end
@@ -282,7 +282,7 @@ module Edusign
       raise GatewayTimeoutError, request.message if request.code == 504
 
       response = Response.new(JSON.parse(request.body, symbolize_names: true))
-      raise Response::ProfessorAlreadyExistsError, response.message if response.message == Response::Error::PROFESSOR_ALREADY_EXISTS_MESSAGE
+      raise Response::ProfessorAlreadyExistsError, response.message if response.message == Response::ProfessorAlreadyExistsError::PROFESSOR_ALREADY_EXISTS_MESSAGE
       raise Response::Error, response.message if response.error?
 
       response
@@ -298,10 +298,11 @@ module Edusign
       class Error < StandardError
         PROFESSOR_DELETED_ERROR_MESSAGE = "professor was deleted".freeze
         CANNOT_GET_PROFESSOR_BY_EMAIL_ERROR_MESSAGE = "Error - cannot get professor by email".freeze
-        PROFESSOR_ALREADY_EXISTS_MESSAGE = "Professor already exists in this school".freeze
       end
 
-      class ProfessorAlreadyExistsError; end
+      class ProfessorAlreadyExistsError
+        PROFESSOR_ALREADY_EXISTS_MESSAGE = "Professor already exists in this school".freeze
+      end
 
       attr_reader :body
 
